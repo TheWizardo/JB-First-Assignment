e8ccd1b515b43ab1671c539197cbb8e893ef741b
TheWizardo <oz.sabbag@gmail.com>
2022-03-23 03:32:36 +0200

    added close button (non functional)

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+const close_button = document.createElement("button");
+close_button.className = "close close-button";
+close_button.ariaLabel = "Close";
+// close_button.addEventListener("click", function () { del_note(this); });
+// close_button.onclick = function () { del_note(this); };
+const inner_span = document.createElement("span");
+inner_span.ariaHidden = "true";
+inner_span.innerHTML = "&times;";
+close_button.appendChild(inner_span);
+
 function loadNotes() {
     let allTasks = JSON.parse(localStorage.getItem("tasks"));
     if (allTasks !== null) {
-        const container = document.getElementById("note_container");
+        const container = document.getElementById("note-container");
         let row = document.createElement("div");
         row.className = "row";
         container.appendChild(row);
@@ -21,8 +31,8 @@ function loadNotes() {
 
 function saveTask() {
     const task_inp = document.getElementById("task");
-    const date_inp = document.getElementById("end_date");
-    const time_inp = document.getElementById("end_time");
+    const date_inp = document.getElementById("end-date");
+    const time_inp = document.getElementById("end-time");
 
     const new_task = {
         task: task_inp.value,
@@ -40,10 +50,8 @@ function saveTask() {
 }
 
 function insertNote(note, row = null) {
-    // TODO
-    // Add close button
     if (row === null) {
-        const container = document.getElementById("note_container");
+        const container = document.getElementById("note-container");
         const rows = container.childNodes;
         row = rows[rows.length - 1];
         if (row.length >= 4) {
@@ -53,6 +61,7 @@ function insertNote(note, row = null) {
         }
     }
     const d = document.createElement("div");
+    d.appendChild(close_button);
     const task_div = document.createElement("div");
     task_div.innerHTML = note.task;
     task_div.className = "task";
@@ -60,4 +69,8 @@ function insertNote(note, row = null) {
     d.className = "col-3 note";
     d.innerHTML += `<br> ${note.date} <br> ${note.time}`;
     row.appendChild(d);
+}
+
+function del_note(btn) {
+    alert(btn);
 }
